a556f4d49b90c2da60caacbc6589067e3ccd57fa
darnell-love27 <101828196+darnell-love27@users.noreply.github.com>
2023-10-26 14:13:43 -0400

    Update NetworkGraph.py

--- NetworkGraph.py
+++ NetworkGraph.py
@@ -3,10 +3,8 @@ import os
 import networkx as nx
 import matplotlib.pyplot as plt
 
-
 # Specify the path to your large JSON file
 file_path = os.path.join(os.getcwd(), "DarnellIPCollect.json")
-
 
 # Function to parse large JSON file and create a network graph
 def parse_large_json(file_path):
@@ -21,13 +19,21 @@ def parse_large_json(file_path):
                 G.add_node(destination_address)
                 G.add_edge(source_address, destination_address)
 
-        # Draw the network graph
-    pos = nx.spring_layout(G)
-    nx.draw(G, pos, with_labels=True, node_size=2000, node_color='skyblue', font_size=10, font_color='black', arrowsize=20)
+        # Create nodes for each IP address
+        for entry in data:
+            source_address = entry["ip_address_host"]
+            G.add_node(source_address)
+            for item in entry["ip_address_attach"]:
+                destination_address = item["ip"]
+                G.add_node(destination_address)
 
-    plt.title('Network of IP Addresses')
-    plt.show()
+        # Draw the network graph with arrows
+        pos = nx.spring_layout(G, k=0.5, iterations=100)
+        plt.figure(figsize=(10, 8))  # Adjust the figure size as needed
+        nx.draw_networkx(G, pos, with_labels=True, node_size=10, node_color='skyblue', font_size=2, font_color='black', arrows=False)
 
+        plt.title('Network of IP Addresses')
+        plt.show()
 
 # Call the function with your file path
 parse_large_json(file_path)
